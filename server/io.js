83eff56bdbdffa972de3dc05553d3f979ba07fc9
Mark Barton <markbarton@marks-mac-mini.home>
2019-05-16 14:10:20 +0100

    lesson 8 baseline

--- server/io.js
+++ server/io.js
@@ -8,45 +8,39 @@ exports.io = function () {
 };
 
 exports.initialize = function (server) {
-  io = sio(server);
-  io.on('connection', (socket) => {
-    logger.debug(`A user connected with ${socket.id}`);
+io = sio(server);
+io.on('connection', (socket) => {
+  logger.debug(`A user connected with ${socket.id}`);
 
-    socket.on('disconnect', function () {
-      // Updated to get real username
-      const user_data = ids.get(socket.id);
-      if (user_data) {
-        logger.debug('USER DISCONNECTED ' + user_data.name)
-      }
-    })
-
-    // Sent from the UserProfile Vue Component
-    socket.on('UPDATE_USER', function (data) {
-      logger.debug(`UPDATE_USER triggered for ${data.name}`)
-      // Map Socket ID with a User
-      users.set(data.name, {
-        socket_id: socket.id,
-        ...data
-      });
-      ids.set(socket.id, data);
-
-      // Also join a room / group - inbuilt socket functionality
-      socket.join(data.group);
-    });
-
-    // Send from the ComposeMessage Vue component
-    socket.on('SEND_MESSAGE', function (data) {
-      // If we have a name then its to a person else group
-      let recipient = '';
-      if (data.name) {
-        const user = users.get(data.name);
-        recipient = user.socket_id;
-      } else {
-        recipient = data.group;
-      }
-      logger.debug(`POPUP_NOTIFICATION triggered for ${recipient}`)
-      io.to(recipient).emit('POPUP_NOTIFICATION', data);
-    });
-
+  socket.on('disconnect', function () {
+    // Updated to get real username
+    const user_data = ids.get(socket.id);
+    if (user_data) {
+      logger.debug('USER DISCONNECTED ' + user_data.name)
+    }
   })
+
+  // Sent from the UserProfile Vue Component
+  socket.on('UPDATE_USER', function (data) {
+    logger.debug(`UPDATE_USER triggered for ${data.name}`)
+    // Map Socket ID with a User
+    users.set(data.name, {
+      socket_id: socket.id,
+      ...data
+    });
+    ids.set(socket.id, data);
+
+    // Also join a room / group - inbuilt socket functionality
+    socket.join(data.group);
+  });
+
+  socket.on('QUIZ_RESPONSE', function (data) {
+    const user_data = ids.get(socket.id);
+    if (user_data) {
+      logger.debug(`${user_data.name} has pressed ${data.response}`);
+    }
+  })
+});
+
+})
 };
--- server/server.js
+++ server/server.js
@@ -7,14 +7,34 @@ const pjson = require("./package.json");
 const path = require('path');
 const http = require('http').Server(app);
 const logger = require('./logger');
+const fs = require('fs');
 
 require('./io').initialize(http);
+const socket = require('./io').io();
 
 const port = process.env.PORT || 8500;
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/send_image', function (req,res){
+    logger.debug(`Send Image Called`);
+    const image_file_path = __dirname + get_random_image();
+    logger.debug(`Sending Image ${image_file_path}`);
+    fs.readFile(image_file_path, function (err, buf) {
+        socket.emit('SHOW_IMAGE', {
+            image: true,
+            buffer: buf.toString('base64')
+        })
+    })
+    res.send();
+})
 
 http.listen(port);
 
 logger.info(`${pjson.name} Server Started >> `);
 logger.info(`running in ${process.env.NODE_ENV}`);
 logger.info(`running on port ${port}`);
+
+function get_random_image(){
+    const index = Math.ceil(Math.random() * (6-1) + 1);
+    return path.normalize(`/images/image${index}.jpg`)
+}
