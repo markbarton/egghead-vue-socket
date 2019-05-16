c7a977fb6aa6c6552c0056153e570417fe176c6d
Mark Barton <markbarton@marks-mac-mini.home>
2019-05-16 16:24:19 +0100

    Vue client lesson 5

---
+++ client/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-toolbar dark fixed app>
+      <v-icon>speaker_phone</v-icon>
+      <v-toolbar-title>Power Vuejs using Socket.io</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <v-layout row mb-4>
+          <v-flex class="mx-1">
+            <UserProfile></UserProfile>
+           </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer dark></v-footer>
+    <PopupMessage></PopupMessage>
+  </v-app>
+</template>
+
+<script>
+import PopupMessage from "@/components/PopupMessage";
+import UserProfile from "@/components/UserProfile";
+export default {
+  components: {
+    PopupMessage,
+    UserProfile
+  }
+};
+</script>
---
+++ client/src/components/PopupMessage.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-snackbar v-model="snackbar" :timeout="timeout" :color="color" top>
+    {{ notification_text }}
+    <v-btn flat @click="snackbar = false">Close</v-btn>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  sockets: {
+    POPUP_NOTIFICATION: function(socket_data) {
+      this.notification_text = socket_data.message;
+      this.color = socket_data.color || "success";
+
+      // Display snackbar
+      this.snackbar = true;
+    }
+  },
+
+  data: () => ({
+    snackbar: false,
+    notification_text: "",
+    timeout: 3000,
+    color: "success"
+  })
+};
+</script>
