d19c08f56d29c27d01e0364a59ca6de59071c270
Mark Barton <markbarton@marks-mac-mini.home>
2019-05-16 16:26:55 +0100

    Vue client

---
+++ client/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <v-toolbar dark fixed app>
+      <v-icon>speaker_phone</v-icon>
+      <v-toolbar-title>Power Vuejs using Socket.io</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <v-layout row mb-4>
+          <v-flex class="mx-1">
+            <UserProfile></UserProfile>
+           </v-flex>
+           <v-flex>
+            <ComposeMessage></ComposeMessage> 
+           </v-flex>  
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer dark></v-footer>
+    <PopupMessage></PopupMessage>
+  </v-app>
+</template>
+
+<script>
+import PopupMessage from "@/components/PopupMessage";
+import UserProfile from "@/components/UserProfile";
+import ComposeMessage from "@/components/ComposeMessage";
+
+export default {
+  components: {
+    PopupMessage,
+    UserProfile,
+    ComposeMessage
+  }
+};
+</script>
